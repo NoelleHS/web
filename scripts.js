4ee769ee3594218cbd4a3e0819c7e3faba2a3352
NoelleHS <nhealeystewart@arizona.edu>
2024-08-10 11:25:59 -0700

    animations

--- scripts.js
+++ scripts.js
@@ -1,93 +1,51 @@
-class SFButton {
-	instance;
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['WELCOME TO COMMAND CENTRAL'], 'text',['white']);
 
-	constructor() {
-		if (!SFButton.instance) SFButton.instance = this;
-		return SFButton.instance;
-	}
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-	initButtons() {
-		document.querySelectorAll(".js-sfbtn").forEach(this.createButton);
-	}
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
 
-	createButton(btn) {
-		if (!btn.classList.contains("js-sfbtn")) exit;
-		const btnContent = btn.innerHTML;
+    } else {
+      con.className = 'console-underscore'
 
-		const btnTail = document.createElement("div");
-		btnTail.classList.add("btn-tail");
-
-		const btnBody = document.createElement("div");
-		btnBody.classList.add("btn-body");
-		btnBody.innerHTML = `<p>${btnContent}</p>`;
-
-		btn.innerHTML = "";
-		btn.classList.add("btn-wrapper");
-		btn.classList.remove("js-sfbtn");
-		btn.appendChild(btnTail);
-		btn.appendChild(btnBody);
-
-		btn.style.setProperty("--bg-color", btn.dataset.bgColor);
-		btn.style.setProperty("--font-color", btn.dataset.fontColor);
-		btn.style.setProperty("--bg-color-hovered", btn.dataset.bgColorHovered);
-		btn.style.setProperty("--font-color-hovered", btn.dataset.fontColorHovered);
-	}
-
-	formatLink(
-		content,
-		href,
-		colorBG,
-		colorBGHovered,
-		colorFont,
-		colorFontHovered
-	) {
-		const link = document.createElement("a");
-		link.dataset.bgColor = colorBG;
-		link.dataset.fontColor = colorFont;
-		link.dataset.bgColorHovered = colorBGHovered;
-		link.dataset.fontColorHovered = colorFontHovered;
-		link.innerHTML = content;
-		link.href = href;
-		link.classList.add("js-sfbtn");
-		return link;
-	}
-}
-
-/*** JavaScript integration ***/
-// First get the SFButton instance
-const sfButton = new SFButton();
-
-// There is two way to use SFButton builder
-
-// When the page has been build ...
-sfButton.initButtons();
-
-// ... And to add a new button asynchronously
-addALinkAsynchronously();
-
-// Those are the functions used to create the asynchronous link
-async function addALinkAsynchronously() {
-	const linkData = await fetchDataFromAnAPIOrWhatEver();
-	const link = sfButton.formatLink(
-		linkData.content,
-		linkData.href,
-		"#fffff0",
-		"#c4afff",
-		"#000000",
-		"#000000"
-	);
-	document.querySelector(".app-body").appendChild(link);
-	sfButton.createButton(link);
-}
-
-function fetchDataFromAnAPIOrWhatEver() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve({
-				content:
-					"About<span style='font-size: 0.7rem; margin-left: 5px;'>Added asynchronously</span>",
-				href: "#"
-			});
-		}, 3000);
-	});
+      visible = true;
+    }
+  }, 400)
 }
